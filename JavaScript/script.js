33576c91a30c3505834753305266a4e1c456b296
Omkar985087 <omkarkadam9422@gmail.com>
2026-06-29 12:05:05 +0530

    Javascript

---
+++ JavaScript/script.js
@@ -0,0 +1,15 @@
+alert("hello world")
+
+console.log("code is running")
+console.log("code is alo running")
+
+var a=prompt("enter your running")
+
+console.log("your number is "+a)
+
+var istrue=confirm("are you sure you want to blast your computer")
+
+if(istrue)
+    console.log("computer is blasting")
+else
+    console.log("computer is not blasting")
