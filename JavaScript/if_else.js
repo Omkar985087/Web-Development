72b6206f0454a79549f96534ded413ceb990c587
Omkar985087 <omkarkadam9422@gmail.com>
2026-06-30 13:18:18 +0530

    Javascript

--- JavaScript/if_else.js
+++ JavaScript/if_else.js
@@ -1,9 +1,21 @@
 console.log("I am conditional tutorial")
 
 let age=45
+// let grace=2;
+
+// console.log(age+grace)
+// console.log(age**grace)
+// console.log(age%grace)
+
 
 if(age>18)
     console.log("you can drive")
 else
     console.log("you cannot driver")
 
+var a=6;
+var b=8
+
+let c=a>b?(a-b):(b-a);
+
+console.log(c)
