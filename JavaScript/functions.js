b54598d82127b34ecbe7ca7bfeb4afbfbab5c497
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-01 14:59:44 +0530

    Javascript

---
+++ JavaScript/functions.js
@@ -0,0 +1,10 @@
+console.log("functions tutorial");
+
+function nice(name) {
+    console.log("hey " + name + " you are a good");
+    console.log("hey " + name + " your t shirt is good");
+    console.log("hey " + name + " your course is good");
+}
+
+nice("omkar");
+nice("rohan");
