bdbb2f1d00731048cbe50801a7440c9bb4e5e88b
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-01 15:19:27 +0530

    Javascript

--- JavaScript/strings.js
+++ JavaScript/strings.js
@@ -11,3 +11,11 @@ let friend="rohan";
 console.log("his name is " + real_name + " and his friend name is " + friend);
 
 console.log(`his name is ${real_name} and his friend name is ${friend}`);
+
+let b="shivam";
+
+console.log(b.toUpperCase());
+
+console.log(b.slice(1,4));
+console.log(b.slice(1));
+console.log(b.concat(real_name));
