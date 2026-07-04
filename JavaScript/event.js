7ccd1b99a63d40175b73e5f215b011afdc91a9ed
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-04 13:13:16 +0530

    Javascript

--- JavaScript/event.js
+++ JavaScript/event.js
@@ -1,16 +1,16 @@
 console.log("events tutorial")
 
-let btn=document.getElementById("btn");
+// let btn=document.getElementById("btn");
 
-btn.addEventListener("dblclick",()=>{
-    document.querySelector(".box").innerHTML="<b> Yayy you were clicked </b>"
-})
+// btn.addEventListener("dblclick",()=>{
+//     document.querySelector(".box").innerHTML="<b> Yayy you were clicked </b>"
+// })
 
 
-btn.addEventListener("contextmenu",()=>{
-    alert("don't hack us");    
-})
+// btn.addEventListener("contextmenu",()=>{
+//     alert("don't hack us");    
+// })
 
-document.addEventListener("keydown",(e)=>{
-    console.log(e.key,e.keyCode);
-})
+// document.addEventListener("keydown",(e)=>{
+//     console.log(e.key,e.keyCode);
+// })
