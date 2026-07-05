fe08659836750d0f8f194df8dd6728bc2341c446
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-05 16:20:11 +0530

    javascript

--- JavaScript/callbacks.js
+++ JavaScript/callbacks.js
@@ -5,3 +5,15 @@ setTimeout(()=>{
 },2000);
 
 console.log("the end");
+
+const callback=(arg)=>{
+    console.log(arg);
+}
+const loadscript=(src,callback) => {
+    let sc=document.createElement("script");
+    sc.src=src;
+    sc.onload=callback("Omkar");
+    document.head.append(sc);
+}
+
+loadscript(src,)
