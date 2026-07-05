d20b544c923d7309c27d04681bb37e3c744c1cf9
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-05 16:53:40 +0530

    javascript

--- JavaScript/promise.js
+++ JavaScript/promise.js
@@ -1,7 +1,11 @@
 console.log('Promise tutorial'); 
 
 let prom1=new Promise((resolve,reject)=>{
-    setTimeout(() => {
+    let a=Math.random()
+    if(a<0.5)
+        reject("No it was not supporting")
+    else
+        setTimeout(() => {
         console.log("Yes i am done");
         resolve("omkar");
     }, 3000);
@@ -9,4 +13,6 @@ let prom1=new Promise((resolve,reject)=>{
 
 prom1.then((a)=>{
     console.log(a);
+}).catch((err)=>{
+    console.log(err);
 })
