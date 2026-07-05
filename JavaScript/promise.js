2b0d2403664d01746904be5681e355c345d14bce
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-05 16:46:51 +0530

    javascript

--- JavaScript/promise.js
+++ JavaScript/promise.js
@@ -1,1 +1,12 @@
-console.log('');    
+console.log('Promise tutorial'); 
+
+let prom1=new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        console.log("Yes i am done");
+        resolve("omkar");
+    }, 3000);
+})
+
+prom1.then((a)=>{
+    console.log(a);
+})
