467b683c2518b6c2ee8ebba32d9fecbc8cca689f
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-06 11:34:13 +0530

    javascript

--- JavaScript/oops.js
+++ JavaScript/oops.js
@@ -1,7 +1,36 @@
 console.log("OOPS tutorial");
 
-let obj={
-    a:1,
-    b:"Omkar"
+// let obj={
+//     a:1,
+//     b:"Omkar"
+// }
+
+// console.log(obj);
+
+// let animal={
+//     eats:true
+// };
+
+// let rabbit={
+//     jumps:true
+// };
+
+// rabbit.__proto__=animal
+
+class animal{
+    constructor(){
+        console.log("object is created")
+    }
+
+    eats()
+    {
+        console.log("kha raha hoon")
+    }
+    jumps()
+    {
+        console.log("khud raha hoon");
+    }
 }
 
+let a=new animal()
+console.log(a);
