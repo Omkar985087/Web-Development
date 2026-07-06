8325253c4415ced08309a1adb3841987c1e0f7f8
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-06 11:15:56 +0530

    javascript

--- JavaScript/error_handling.js
+++ JavaScript/error_handling.js
@@ -1,18 +1,40 @@
-let a=prompt("enter first number");
+let a = prompt("enter first number");
 
-let b=prompt("enter second number");
+let b = prompt("enter second number");
 
-if(isNaN(a) || isNaN(b))
-{
+if (isNaN(a) || isNaN(b)) {
     throw SyntaxError("sorry this is not allowed");
 }
 
-let sum=parseInt(a)+parseInt(b);
+let sum = parseInt(a) + parseInt(b);
 
 
-try{
-    console.log("the sum is",sum*x);
+// try{
+//     console.log("the sum is",sum*x);
+// }
+// catch(error){
+//     console.log("error aa gaya bhai");
+// }
+
+// finally{
+//     console.log("files are being closed and db connection is being closed")
+// }
+
+let x=1;
+function main() {
+    try {
+        console.log("the sum is", sum * x);
+        return true;
+    }
+    catch (error) {
+        console.log("error aa gaya bhai");
+        return false;
+    }
+
+    finally {
+        console.log("files are being closed and db connection is being closed")
+    }
 }
-catch(error){
-    console.log("error aa gaya bhai");
-}
+
+let c = main();
+console.log(c);
