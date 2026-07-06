4a7e34339e6892efecdaba254bb3442d1422b7f6
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-06 11:09:30 +0530

    javascript

--- JavaScript/error_handling.js
+++ JavaScript/error_handling.js
@@ -2,6 +2,17 @@ let a=prompt("enter first number");
 
 let b=prompt("enter second number");
 
+if(isNaN(a) || isNaN(b))
+{
+    throw SyntaxError("sorry this is not allowed");
+}
+
 let sum=parseInt(a)+parseInt(b);
 
-console.log(sum);
+
+try{
+    console.log("the sum is",sum*x);
+}
+catch(error){
+    console.log("error aa gaya bhai");
+}
