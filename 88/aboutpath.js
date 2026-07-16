9a37c34888dfb5f736e1a89f9d90d81e8aed7075
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-16 17:50:13 +0530

    backend

---
+++ 88/aboutpath.js
@@ -0,0 +1,8 @@
+import path from "path"
+
+let mypath="C:\\Users\\Omkar Kadam\\Desktop\\Web development\\88\\omkar.txt"
+
+console.log(path.extname(mypath));
+console.log(path.dirname(mypath));
+
+console.log(path.join("c:/","programs\\omkar.txt"))
