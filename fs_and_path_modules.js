0d37e22e60717f9a64218631f374dbe7c399f052
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-16 15:56:31 +0530

    backend

--- fs_and_path_modules.js
+++ fs_and_path_modules.js
@@ -1,5 +1,3 @@
-import http from "http"
-
 const { createServer } = require('node:http');
 
 
@@ -7,8 +5,8 @@ const hostname = '127.0.0.1';
 const port = 3000;
 const server = createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1> Hello World </h1>');
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('hello world');
 }); 
 
 server.listen(port, hostname, () => {
