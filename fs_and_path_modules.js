768a008c3d3a5a80c28d8b8d0949b1b000dc11bf
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-16 16:12:43 +0530

    backend

--- fs_and_path_modules.js
+++ fs_and_path_modules.js
@@ -1,14 +1,18 @@
-const { createServer } = require('node:http');
+import http from "http"
+// const { createServer } = require('node:http');
 
+// const hostname = '127.0.0.1';
+// const port = 3000;
+// const server = createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('hello world');
+// }); 
 
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('hello world');
-}); 
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+import {a} from "./myserver.js"
+
+console.log(a)
