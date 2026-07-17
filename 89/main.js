c6f0e2e7182bc20e77a1cb2d1cb1ffb20b564017
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-17 11:58:54 +0530

    Express JS

--- 89/main.js
+++ 89/main.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'))
 //app.get or app.post or app.put or app.delete(path,handler)
 app.get('/', (req, res) => {
   res.send('Hello World!');
