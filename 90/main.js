d0765f8e1ccaf323a5ce79b83e4f587917a506b8
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-17 16:14:14 +0530

    get,post,put and delete

--- 90/main.js
+++ 90/main.js
@@ -1,22 +1,33 @@
 const express = require('express');
+const blog = require('./routes/blog');
+
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'))
+app.use('/blog', blog);
+
 app.get('/', (req, res) => {
     console.log("hey it's a get request")
     res.send('Hello World!');
-});
-
-app.post('/', (req, res) => {
+}).post('/', (req, res) => {
     console.log("hey it's a post request")
-    res.send('Hello World!');
+    res.send('Hello World post!');
+}).put('/', (req, res) => {
+    console.log("hey it's a put request")
+    res.send('Hello World put!');
 });
 
-app.put('/', (req, res) => {
-    console.log("hey it's a put request")
-    res.send('Hello World!');
+app.get("/index1",(req, res) => {
+    console.log("hey it's a index")
+    res.sendFile('template/index1.html',{root:__dirname})
 });
+
+app.get("/api",(req, res) => {
+    res.json({a:1,b:2,c:3})
+});
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
