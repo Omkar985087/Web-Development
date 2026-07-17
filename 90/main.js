2e62081919852804c8df6b99ea6237bac7e6ae01
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-17 12:27:26 +0530

    Express JS

--- 90/main.js
+++ 90/main.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'))
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    console.log("hey it's a get request")
+    res.send('Hello World!');
+});
+
+app.post('/', (req, res) => {
+    console.log("hey it's a post request")
+    res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
