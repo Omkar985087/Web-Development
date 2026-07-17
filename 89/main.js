32c88cac8a63fd3daacf98424c0064a800ce0c48
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-17 11:29:38 +0530

    Express JS

--- 89/main.js
+++ 89/main.js
@@ -2,8 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//app.get or app.post or app.put or app.delete(path,handler)
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About us!');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('Hello Contact me!');
+});
+
+app.get('/blog', (req, res) => {
+  res.send('Hello Blog!');
 });
 
 app.listen(port, () => {
