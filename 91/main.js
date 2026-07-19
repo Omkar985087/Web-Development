39dc8b8c0704999a2dc865d9ad3a902dc3f1e649
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-19 11:37:06 +0530

    Middleware

--- 91/main.js
+++ 91/main.js
@@ -1,7 +1,10 @@
 const express=require("express")
 const app =express()
+const birds = require('./routes/birds');
 const port=3000
 const fs=require("fs")
+
+app.use('/birds', birds);
 
 // app.use(express.static("public"))
 
