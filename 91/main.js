e6538594898210bb5a8094a2cda11c00b3cc0219
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-19 11:12:28 +0530

    Middleware

--- 91/main.js
+++ 91/main.js
@@ -2,10 +2,19 @@ const express=require("express")
 const app =express()
 const port=3000
 
-app.get('/',(req,pos)=>{
-    res.send('Hello world')
+app.use(express.static("public"))
+
+app.get('/',(req,res)=>{
+    res.send('Hello world!')
 })
 
+app.get('/about',(req,res)=>{
+    res.send('Hello contact')
+})
+
+app.get('/contact',(req,res)=>{
+    res.send('Hello contact!')
+})
 app.listen(port,()=>{
     console.log(`example app listening on port ${port}`)
 })
