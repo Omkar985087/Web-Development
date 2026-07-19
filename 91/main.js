30562f5029b2fa3b5d96e4e6cd0febb6faaa9c89
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-19 11:17:07 +0530

    Middleware

--- 91/main.js
+++ 91/main.js
@@ -2,7 +2,19 @@ const express=require("express")
 const app =express()
 const port=3000
 
-app.use(express.static("public"))
+// app.use(express.static("public"))
+
+//middleware 2
+app.use((req,res,next)=>{
+    console.log("M1")
+    next()
+})
+
+//middleware 2
+app.use((req,res,next)=>{
+    console.log("M2")
+    next()
+})
 
 app.get('/',(req,res)=>{
     res.send('Hello world!')
