84e952250e18db9b45a226d3b3e8c64123626f72
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-21 11:09:58 +0530

    CRUD

--- 95/crud.mongodb.js
+++ 95/crud.mongodb.js
@@ -9,3 +9,63 @@ db.courses.insertOne({
     assignments:12,
     projects:45
 })
+
+db.courses.insertMany([
+  {
+    name: "JavaScript Mastery",
+    price: 499,
+    assignments: 15,
+    projects: 10
+  },
+  {
+    name: "React Complete Guide",
+    price: 999,
+    assignments: 20,
+    projects: 18
+  },
+  {
+    name: "Node.js Backend Development",
+    price: 799,
+    assignments: 16,
+    projects: 12
+  },
+  {
+    name: "MongoDB for Beginners",
+    price: 299,
+    assignments: 10,
+    projects: 8
+  },
+  {
+    name: "Full Stack MERN Bootcamp",
+    price: 1499,
+    assignments: 30,
+    projects: 25
+  },
+  {
+    name: "Python Programming",
+    price: 699,
+    assignments: 18,
+    projects: 15
+  },
+  {
+    name: "Data Structures & Algorithms",
+    price: 899,
+    assignments: 40,
+    projects: 5
+  },
+  {
+    name: "Machine Learning Basics",
+    price: 1299,
+    assignments: 22,
+    projects: 14
+  },
+  {
+    name: "DevOps Essentials",
+    price: 999,
+    assignments: 17,
+    projects: 11
+  }
+]);
+
+let a=db.courses.find({price:0})
+console.log(a)
