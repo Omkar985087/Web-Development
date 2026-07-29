57305c83dd773d9a98b580c224bb5e5b47e6fced
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-29 16:51:25 +0530

    react.js introduction

--- 105/with-react/src/App.js
+++ 105/with-react/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import { use, useState } from 'react';
 import "./App.css"
+import Navbar from './components/navbar'
 
 function App() {
   const [value,setvalue]=useState(0);
   return (
     <div className="App">
+      <Navbar/>
       <div className='value'>{value}</div>
       <button onClick={()=>{setvalue(value+1)}}>click me</button>
     </div>
