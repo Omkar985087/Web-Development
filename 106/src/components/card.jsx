b685324df3a25001314cdb6615cda719f04b7a99
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-31 12:18:24 +0530

    Components,props and JSX

--- 106/src/components/card.jsx
+++ 106/src/components/card.jsx
@@ -3,6 +3,7 @@ import "./card.css"
 const card = (props) => {
   return (
     <div className='card'>
+        <img src="https://media.kingston.com/kingston/hero/ktc-articles-solutions-speed-up-your-mac-hero-lg.jpg" alt="" width={307} style={{border:"2px solid black",overflow:"hidden"}} />
         <h1>{props.title}</h1>
         <p>{props.description}</p>
     </div>
