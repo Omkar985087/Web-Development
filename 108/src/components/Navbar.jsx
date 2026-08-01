16fdff63b68f07cc6bef1fb4adbd62fdaccba5d6
Omkar985087 <omkarkadam9422@gmail.com>
2026-08-01 12:12:46 +0530

    useeffect

--- 108/src/components/Navbar.jsx
+++ 108/src/components/Navbar.jsx
@@ -1,9 +1,22 @@
 import react from 'react'
 import { useEffect } from 'react'
 const Navbar=({color})=>{
+
+    //case 1:Run on every render
     useEffect(()=>{
-        alert("color was changed")
+        alert("hey i will run on every render")
+    },[])
+
+    //case 2:Run only on first render
+    useEffect(()=>{
+        alert("hey welcome tom my page.this is first render")
+    },[])
+
+    //case 3:Run only when certain vlaues change
+    useEffect(()=>{
+        alert("hey i am running because color was changed")
     },[color])
+
     return(
         <div>
             I am navbar {color} hehhe
